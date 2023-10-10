c784ae7eb125e31c243eb6e3bf7c3081fadddeb4
Ploy <Ploypailin_mt@hotmail.com>
2023-10-10 14:45:22 +0700

    feat: Finish Assignment Ex 1 & 2

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,13 @@
 //Exercise #1: Variable Creation
-//Start coding here
+let name = "Karter";
+let lastName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+let company = "Chef's Table by Chef James";
+let price = 15000;
+let quantityNumber = 1500;
 
 //Exercise #2: Variable Reassignment
-//Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+price = 5000;
+quantityNumber = 2500;
